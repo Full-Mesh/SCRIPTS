66bc20d3e722b9ad8a5dda1846dbcfa73f05e166
Paul Stroud <124333025+paulstroud2023@users.noreply.github.com>
2023-04-05 09:38:40 -0700

    functional

---
+++ static ip hostname v2.ps1
@@ -0,0 +1,27 @@
+﻿# this script sets a static IP address for the ethernet adapter
+# and the hostname according to the variables below
+    
+$newname = "GRNGNS-WINSVR" # new hostname
+$newip = "10.11.11.123"    # new ip address
+$newvlsm = 27              # subnet mask in slash notation
+$newgtwy = "10.11.11.126"  # new default gateway
+
+
+$adapter = Get-NetAdapter  # extract NIC index
+Write-Host "Adapter index: $($adapter[0].ifIndex)"
+
+Remove-NetIPAddress -InterfaceIndex $($adapter[0].ifIndex) -Confirm:$false
+Remove-NetRoute -InterfaceIndex $($adapter[0].ifIndex) -Confirm:$false
+
+
+# set the static IP using the variables as inputs
+New-NetIPAddress -InterfaceIndex $adapter[0].ifIndex -IPAddress $newip -PrefixLength $newvlsm -DefaultGateway $newgtwy
+#Set-NetIPInterface -InterfaceAlias $adapter[0].Name -DefaultGateway $newgtwy 
+
+# change the hostname
+Rename-Computer -NewName $newname
+
+sleep 1
+ipconfig
+hostname
+
